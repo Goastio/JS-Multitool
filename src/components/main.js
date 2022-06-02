0d8f68c543a063a623a39fc8810e7ea589c5347f
rlogank <rlogank@icloud.com>
2022-06-02 17:55:46 -0400

    changed some colors

--- src/components/main.js
+++ src/components/main.js
@@ -20,12 +20,12 @@ function Main() {
 
 
 
-      <div className="h-3/4 w-1/2 bg-[#2A2A2A] border-2 border-red-500 sm:flex justify-center items-center gap-8 px-8">
+      <div className="h-3/4 w-1/2 bg-blue-500 border-2 border-red-500 sm:flex justify-center items-center gap-8 px-8">
 
 
 
-    <div className="h-1/2 w-1/2 bg-[#1A1A1A] flex justify-center items-center rounded-md">hi</div>
-    <div className="h-1/2 w-1/2 bg-[#1A1A1A] flex justify-center items-center rounded-md">hi</div>
+    <div className="h-1/2 w-1/2 bg-white flex justify-center items-center rounded-md">hi</div>
+    <div className="h-1/2 w-1/2 bg-white flex justify-center items-center rounded-md">hi</div>
 
 
       </div>
